92d2994bf359100d0a6be7afd63caee9c1640bfc
rohan27hadvani <rohan.hadvani0@gmail.com>
2024-12-17 20:27:47 +0530

    Auth Complete

---
+++ AppwriteTest/src/config/config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+const config = {
+    appwriteProjectID : String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabaseID : String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionID : String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketID : String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
+    
+}
+
+export default config
